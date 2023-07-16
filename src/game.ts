ac74f4ce652da5cba75fcc2d802ff3678b27b90c
Emre Guneyler <eguneys@gmail.com>
2023-07-16 13:17:14 +0300

    anims cards

--- src/game.ts
+++ src/game.ts
@@ -44,32 +44,80 @@ export class RectView extends Play {
   }
 }
 
+const ranks = '2tjka'.split('')
+const suits = 'rb'.split('')
+
+const cards = ranks.flatMap(rank => suits.map(suit => rank + suit))
+
+type OCard = string
 
 class Card extends Play {
+
+  bg!: Anim
+  r!: Anim
+
+
   _init() {
-    let a = this.make(Anim, Vec2.zero, {
+    this.bg = this.make(Anim, Vec2.zero, {
       name: 'card_bg'
     })
-    a.origin = Vec2.make(16, 20)
-    a.play_now('spawn', () => {
-      a.play_now('idle')
+    this.bg.origin = Vec2.make(16, 20)
+    this.bg.visible = false
+    
+    this.r = this.make(Anim, Vec2.zero, {
+      name: 'rank'
+    })
+    this.r.origin = Vec2.make(12, 16)
+    this.r.visible = false
+
+  }
+
+  spawn(card?: OCard) {
+    this.bg.visible = true
+    this.bg.play_now('spawn', () => {
+      if (card) {
+        this.bg.play_now('idle')
+        this.r.play_now(card)
+        this.r.visible = true
+      } else {
+        this.bg.play_now('back')
+      }
     })
   }
 }
 
 class StackSizePlay extends Play {
+
+  me_cards!: [Card, Card]
+  op_cards!: [Card, Card]
+
   _init() {
 
     this.make(RectView, Vec2.zero, { w: 320, h: 180, color: Color.hex(0x101088)})
 
-    this.make(Card, Vec2.make(30, 154), {})
-    this.make(Card, Vec2.make(64, 154), {})
+    this.me_cards = [
+      this.make(Card, Vec2.make(30, 154), {}),
+      this.make(Card, Vec2.make(64, 154), {})
+    ]
 
 
 
-    this.make(Card, Vec2.make(256, 24), {})
-    this.make(Card, Vec2.make(290, 24), {})
+    this.op_cards = [
+      this.make(Card, Vec2.make(256, 24), {}),
+      this.make(Card, Vec2.make(290, 24), {})
+    ]
 
+    this.deal(cards.slice(0, 2))
+
+  }
+
+
+  deal(card: [OCard, OCard]) {
+    this.me_cards[0].spawn(card[0])
+    this.me_cards[1].spawn(card[1])
+
+    this.op_cards[0].spawn()
+    this.op_cards[1].spawn()
   }
 }
 
